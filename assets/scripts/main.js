ad24900df8a0cc58dd5af22e96be42a37dc7d598
marcodldev <marcodelisi2@gmail.com>
2022-10-22 18:33:48 +0200

    fixed

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -10,32 +10,35 @@ for (let i = 1; i < 101; i++) {
 
      let scatola = document.createElement("div");
      scatola.classList.add("box");
-     scatola.classList.add("g-2");
-     scatola.classList.add("container");
-     if ( i % 3 == 0) {
-      scatola.innerHTML = `Fizz`;
-      scatola.classList.add("fizz");
-     } else if ( i % 5 == 0 ) {
-
-      scatola.innerHTML = `Buzz`;
-      scatola.classList.add("buzz");
-
-     }  else if ( i % 3 && i % 5 == 0 ) {
-
+     scatola.classList.add("text-center");
+     scatola.classList.add("align-items-center");
+    //  scatola.classList.add("container");
+    if ( i % 15 == 0) {
       scatola.innerHTML = `FizzBuzz!`;
       scatola.classList.add("fizzbuzz");
       console.log("FizzBuzz")
+    }
 
-     } else {
+    else if ( i % 3 == 0) {
+      scatola.innerHTML = `Fizz`;
+      scatola.classList.add("fizz");
+    }
 
+    else if ( i % 5 == 0) {
+      scatola.innerHTML = `Buzz`;
+      scatola.classList.add("buzz");
+    }
+
+    else {
       scatola.innerHTML = `${i}`
       scatola.classList.add("numeri");
+    }
 
-     }
-  
      {listaJs.append(scatola);}
 
 }
+
+
   
 
 
